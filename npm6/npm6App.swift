0201349e4e891c43267d66ea11e1f6c0bd13f22e
Morris Albers <dagger5@alberslegal.com>
2021-04-08 17:28:45 -0500

    Initial Commit

---
+++ npm6/npm6App.swift
@@ -0,0 +1,17 @@
+//
+//  npm6App.swift
+//  npm6
+//
+//  Created by Morris Albers on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct npm6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
